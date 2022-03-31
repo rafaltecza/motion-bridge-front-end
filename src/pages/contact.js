f0eb4964ffdcffbd1c564b202da15647a954cc8c
Bartek <bartosz.bizon@reddeergames.com>
2022-03-31 20:28:12 +0200

    Add Container to Contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className={"container"} style={{
+            minHeight: '90vh'
+        }}>
+            <div>
+                <h1>Contacts</h1>
+            </div>
         </div>
     );
 };
