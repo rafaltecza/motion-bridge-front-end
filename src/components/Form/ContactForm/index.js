d7302d78bc842f2cb321750ffa1417fe91903926
Bartek <bartosz.bizon@reddeergames.com>
2022-05-07 17:58:10 +0200

    Add Email Regex,Contact Form Validation

--- src/components/Form/ContactForm/index.js
+++ src/components/Form/ContactForm/index.js
@@ -1,7 +1,7 @@
-import Navbar from "../../Navbar";
-import React from 'react';
-import {Button, Checkbox, FormControlLabel, FormGroup, TextareaAutosize, TextField} from "@mui/material";
-import {red} from "@mui/material/colors";
+import React from "react";
+import {FormGroup, TextareaAutosize, TextField} from "@mui/material";
+import {SnackbarProvider, useSnackbar} from "notistack";
+import SnackbarHandler from './SnackbarHandler'
 
 export default class ContactForm extends React.Component {
 
@@ -9,15 +9,18 @@ export default class ContactForm extends React.Component {
         super(props);
         this.state = {
             email: '',
+            helperText:'',
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+
     handleInputChange(event) {
         const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const value = target.value;
         const name = target.name;
 
         this.setState({
@@ -25,12 +28,55 @@ export default class ContactForm extends React.Component {
         });
     }
 
-    handleSubmit(event) {
-        alert('Email send: ' + this.state.email);
-        event.preventDefault();
+    validateEmail(emailToValidate)
+    {
+        if(emailToValidate.length == 0)
+            return true;
+
+        const regex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        let result = regex.test(String(emailToValidate).toLowerCase());
+        return result;
     }
 
+
+
+    handleEmailChange(event){
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        if(!this.validateEmail(value))
+        {
+            this.state.helperText="Incorrect Email"
+        }
+        else
+        {
+            this.state.helperText=""
+        }
+
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    validateForm(name,email,message)
+    {
+        let emailCorrect = this.validateEmail(email)
+        let emailIsEmpty = email.length == 0;
+        if(name!=null && emailCorrect && message!=null && !emailIsEmpty)
+            return true;
+        else
+            return false;
+    }
+
+    handleSubmit(event) {
+            //alert('Email send: ' + this.state.email);
+            event.preventDefault();
+    }
+
+
     render() {
+
         return (
             <form onSubmit={this.handleSubmit}>
                 <FormGroup >
@@ -55,8 +101,10 @@ export default class ContactForm extends React.Component {
                                            type="text"
                                            name={"email"}
                                            value={this.state.email}
-                                           onChange={this.handleInputChange}
-                                           required/>
+                                           onChange={this.handleEmailChange}
+                                           required
+                                           error={!this.validateEmail(this.state.email)}
+                                           helperText={this.state.helperText}/>
                             </div>
                         </div>
                         <div className={"col-6"}>
@@ -76,8 +124,13 @@ export default class ContactForm extends React.Component {
                         </div>
                     </div>
                 </FormGroup>
-                <input className={"float-end button-red"} type="submit" value="Send" />
+                <SnackbarProvider maxSnack={3}>
+                    <SnackbarHandler
+                        error={this.validateForm(this.state.name,this.state.email,this.state.message)}>
+                    </SnackbarHandler>
+                </SnackbarProvider>
             </form>
+
         );
     }
 }
