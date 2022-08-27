8a397715e1c952b7aa3b395df222a75aa9c876d0
Rafał Tęcza <rafaltecza@MacBook-Pro-Rafa.local>
2022-08-27 02:33:19 +0200

    (🔋 refactor): styles of the page

--- src/components/Card/CardElements.jsx
+++ src/components/Card/CardElements.jsx
@@ -3,6 +3,14 @@ import {Card} from "@material-ui/core";
 
 export const CardElement = styled(Card)`
   &.MuiPaper-root {
-    background-color: rgba(255, 255, 255, 0.9);
+    border: none;
+    border-radius: 5px !important;
+    z-index: 0;
+    background-color: rgba(21, 21, 21, .9);
+    color: snow;
+
+    div {
+      color: snow;
+    }
   }
 `;
