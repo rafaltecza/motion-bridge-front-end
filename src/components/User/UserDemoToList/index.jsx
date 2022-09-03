13fe1f0a2578d6dc7416f2da0e761e2c12e6db95
Bartek <bartoszbizon97@gmail.com>
2022-09-03 22:02:46 +0200

    Improve GetUsers List

--- src/components/User/UserDemoToList/index.jsx
+++ src/components/User/UserDemoToList/index.jsx
@@ -1,5 +1,6 @@
 import ParticlesBg from "particles-bg";
 import React from "react";
+import {Grid} from "@mui/material";
 
 const UserDemoToList = (props) => {
     return (
@@ -20,15 +21,22 @@ const UserDemoToList = (props) => {
                         <div className={"card-body text-white p-4 z-1"}>
                             <div className={"d-flex"}>
                                 <div className={"row g-0 w-100"}>
-                                    <div className={"col-auto"}>
-                                        <h3 className={"me-3"}>Username: {props.name} , </h3>
-                                    </div>
-                                    <div className={"col-auto"}>
-                                        <h3 className={"me-3"}>Email: {props.label} , </h3>
-                                    </div>
-                                    <div className={"col-auto"}>
-                                        <h3 className={"me-3"}>Is Verified: {props.id}</h3>
-                                    </div>
+
+                                    <Grid container spacing={3}>
+                                        <Grid item xs={4}>
+                                            <h5>Name:</h5>
+                                            <span>{props.name}</span>
+                                        </Grid>
+                                        <Grid item xs={4}>
+                                            <h5>Email:</h5>
+                                            <span>{props.label}</span>
+                                        </Grid>
+                                        <Grid item xs={4}>
+                                            <h5>Account Activated:</h5>
+                                            <span>{props.id}</span>
+                                        </Grid>
+                                    </Grid>
+
                                 </div>
                             </div>
                             <div className={"col-12 col-md-8"}>
